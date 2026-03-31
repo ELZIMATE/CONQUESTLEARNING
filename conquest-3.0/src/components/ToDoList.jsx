1bf41e9164f6eefdd2920e70334e2ab1df804200
ELZIMATE <eliaszakharia33@gmail.com>
2026-03-31 15:34:05 +1100

    SHIPPED CONQUEST LIST BACKEND

--- conquest-3.0/src/components/ToDoList.jsx
+++ conquest-3.0/src/components/ToDoList.jsx
@@ -1,27 +1,63 @@
 import { useState } from "react"
 import NavBar from "./Navbar"
 import useFeatures from "../hooks/useFeatures"
+import { useEffect } from "react"
+import {fetchList, addList} from "../hooks/backendapi/Featuresapi"
 
-const ToDoList = ({list, setList, ...feats}) => {
+
+const ToDoList = ({list, setList, user, ...feats }) => {
+
+    console.log('list value:', list, 'type:', typeof list)
 
 
     const[task, setTask] = useState('')
 
 
 
-    const listadder = () => { 
+    useEffect(() => {
 
-        setList([...list, task])
-        setTask('')
+        const LISTMAKE = async() => {
+            const data = await fetchList()
+            if (data){
+
+                setList(Array.isArray(data.ToDos) ? data.ToDos : [])
+                }
+    
+            
+        }
+        
+        LISTMAKE()
+
+
+
+    }, [])
+
+
+
+    const Listadd = async() => {
+        const today = new Date().toISOString().split('T')[0] //today box will hold current date
+        const thing = {
+            date: today,
+            user_id: user.id,
+              ToDos: [...(list || []), task]
+            
+
+        }
+
+       await addList(thing)
+
+        setList(prev => [...prev, task])
+setTask('')
+
     }
 
 return(
     <>
     <input value={task} onChange={(e) => setTask(e.target.value)}></input>
-    <button onClick={listadder}>Add to List</button>
+    <button onClick={Listadd}>Add to List</button>
     
 
-    {list.map((lis) => 
+    {(list || []).map((lis) => 
     
     <li key={lis}>
 
--- conquest-3.0/src/hooks/backendapi/Featuresapi.js
+++ conquest-3.0/src/hooks/backendapi/Featuresapi.js
@@ -24,6 +24,46 @@ return data
 }
 
 
+export const fetchList = async() => {
+  const { data: { session } } = await supabase.auth.getSession()
+    if (!session?.user) {
+    throw new Error("No active Supabase session. Log in again before saving timer data.")
+
+}
+
+const today = new Date().toISOString().split('T')[0]
+
+const{error, data} = await supabase
+.from("daily_logs")
+.select("*")
+.eq('user_id', session.user.id)
+.eq('date', today)
+.single()
+
+
+if (error) throw error
+return data
+}
+
+export const addList = async(thing) => {
+
+  const { data: { session } } = await supabase.auth.getSession()
+  console.log('session in addtimer:', session)
+
+  if (!session?.user) {
+    throw new Error("No active Supabase session. Log in again before saving timer data.")
+  }
+
+    const{ data, error} = await supabase
+    .from("daily_logs")
+    .upsert([thing], {onConflict: 'user_id, date'})
+        
+       if (error) throw error
+       return data 
+    
+
+}
+
 
 
 
