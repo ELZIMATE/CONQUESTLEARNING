f1749fc1316531f2f7f1f0c0db9361324f4b7ed6
ELIASZAK <youremail@email.com>
2026-05-15 22:01:50 +1000

    NOTES UPDATES

--- conquest-3.0/src/hooks/backendapi/Featuresapi.js
+++ conquest-3.0/src/hooks/backendapi/Featuresapi.js
@@ -2,8 +2,11 @@ import { supabase } from "../../assets/features/tools/supabaseClient"
 
 const getDateOrToday = (selectedDate) => selectedDate ?? new Date().toISOString().split('T')[0]
 
-const updateOrInsert = async(table, payload, match) => {
-  const updatePayload = { ...payload }
+const updateOrInsert = async(table, payload, match) => { //update or insert function that takes in the 
+// table we want to update or insert into, this works brackets to brackets when Update or Insert is called within the functions callback to match and puts in the table we name from the backend into the table const param, same goes for the others
+//payload const param gets filled with the whole thing object storing the whole todos list. 
+// the payload which is the data we want to update or insert, and the match which is the criteria for finding the row to update, match is filled with the user id and date to find the specific row for that user and date
+  const updatePayload = { ...payload } //
   Object.keys(match).forEach((key) => {
     delete updatePayload[key]
   })
@@ -23,7 +26,7 @@ const updateOrInsert = async(table, payload, match) => {
 
   const { data, error } = await supabase
     .from(table)
-    .insert([payload])
+    .upsert([payload])
     .select()
 
   if (error) throw error
